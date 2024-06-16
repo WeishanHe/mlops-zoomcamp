d0c746b5d7b33b5bee25c8ab34f31ab4467d0a9d
Weishan He <61961870+WeishanHe@users.noreply.github.com>
2024-06-16 11:03:43 -0400

    add new folder

--- 04-deployment/web-service/test.py
+++ 04-deployment/web-service/test.py
@@ -9,3 +9,15 @@ ride = {
 url = 'http://localhost:9696/predict'
 response = requests.post(url, json=ride)
 print(response.json())
+
+# import predict
+
+# ride = {
+#     "PULocationID": 10,
+#     "DOLocationID": 50,
+#     "trip_distance": 40
+# }
+
+# features = predict.prepare_features(ride)
+# pred = predict.predict(features)
+# print(pred)
